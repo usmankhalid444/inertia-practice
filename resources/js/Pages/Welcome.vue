568e564713ba9027128b09bafdd5770a6fe98833
usman <usman.khalid444@gmail.com>
2021-12-03 05:58:16 +0500

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello Woffsrlddd {{ name }}</h1>
+    <ul>
+        <li
+            v-for="framwork of framworks"
+            v-text="framwork"
+            :key="framwork"
+        ></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        framworks: Array,
+    },
+};
+</script>
